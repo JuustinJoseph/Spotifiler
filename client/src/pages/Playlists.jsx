081340617627fbb782b383708288fb97f2ec36fe
Justin Joseph <justinjoseph2336@gmail.com>
2025-03-03 16:01:55 -0500

    basic setup

---
+++ client/src/pages/Playlists.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Playlists = () => {
+  return (
+    <>
+      <h1>Playlists</h1>
+    </>
+  );
+};
+
+export default Playlists;
---
+++ client/src/pages/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfilePage = () => {
+  return (
+    <div className="">
+      <h1 className="">Profile</h1>
+    </div>
+  );
+};
+
+export default ProfilePage;
